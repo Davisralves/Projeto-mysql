743cc31527fd8fc43471d29829dfe551f1146f69
Davisralves <davialves324@gmail.com>
2022-01-16 13:23:10 -0300

    finalizando requisito 10

---
+++ desafio10.sql
@@ -0,0 +1,10 @@
+SELECT cancoes.nome AS nome,
+COUNT(historico.music_id) AS reproducoes
+FROM SpotifyClone.cancoes AS cancoes
+LEFT JOIN SpotifyClone.historico as historico
+ON cancoes.cancao_id = historico.music_id
+LEFT JOIN spotifyclone.usuario
+ON historico.usuario_id = usuario.usuario_id
+WHERE usuario.plano_id = 1 OR usuario.plano_id = 4
+GROUP BY historico.music_id
+ORDER BY cancoes.nome;
