8fe2f589d9ebea47e52a5ff0da5c65a7b2c91837
Davisralves <davialves324@gmail.com>
2022-01-16 12:15:58 -0300

    finalizandor equisito 7

---
+++ desafio7.sql
@@ -0,0 +1,9 @@
+SELECT artistas.nome AS artistas,
+album.album as album,
+COUNT(usuario_artista.usuario_id) AS seguidores
+FROM SpotifyClone.album AS album
+LEFT JOIN SpotifyClone.artistas AS artistas
+ON artistas.artista_id = album.artista_id
+LEFT JOIN SpotifyClone.usuario_artista AS usuario_artista
+ON usuario_artista.artista_id = album.artista_id
+GROUP BY album.album;
