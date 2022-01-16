aebe2179260527dc1153a32998b3b3654d8ababd
Davisralves <davialves324@gmail.com>
2022-01-16 11:40:02 -0300

    finalizando requisito 6

---
+++ desafio6.sql
@@ -0,0 +1,7 @@
+SELECT MIN(plano.valor) AS faturamento_minimo,
+MAX(plano.valor) AS faturamento_maximo,
+TRUNCATE(AVG(plano.valor), 2) AS faturamento_medio,
+TRUNCATE(SUM(plano.valor), 2) AS faturamento_total
+FROM SpotifyClone.plano AS plano
+RIGHT JOIN SpotifyClone.usuario AS usuario
+ON plano.plano_id = usuario.plano_id;
