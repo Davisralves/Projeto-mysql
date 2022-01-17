526392ce2768bef4625b13e46afec7fbbd99c56d
Davisralves <davialves324@gmail.com>
2022-01-17 11:07:23 -0300

    corrigindo requisito 3

--- desafio3.sql
+++ desafio3.sql
@@ -1,11 +1,11 @@
 SELECT 
 usuario.nome AS usuario, 
 COUNT(historico.usuario_id) AS qtde_musicas_ouvidas,
-SUM(FLOOR(cancoes.duracao / 60, 2)) AS total_minutos
+ROUND(SUM(cancoes.duracao / 60), 2) AS total_minutos
 FROM SpotifyClone.usuario as usuario
 LEFT JOIN SpotifyClone.historico as historico
 ON usuario.usuario_id = historico.usuario_id
 INNER JOIN SpotifyClone.cancoes as cancoes
-ON cancoes.nome = historico.historico_de_reproducao
+ON cancoes.cancao_id = historico.music_id
 GROUP BY usuario.nome
 ORDER BY usuario.nome;
