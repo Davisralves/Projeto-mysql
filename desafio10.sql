39b06070f9d9645b1f3b4e7615e9b8889be8fcaa
Davisralves <davialves324@gmail.com>
2022-01-17 11:18:12 -0300

    corrigindo nome do banco para  SpotifyClone

--- desafio10.sql
+++ desafio10.sql
@@ -3,7 +3,7 @@ COUNT(historico.music_id) AS reproducoes
 FROM SpotifyClone.cancoes AS cancoes
 LEFT JOIN SpotifyClone.historico as historico
 ON cancoes.cancao_id = historico.music_id
-LEFT JOIN spotifyclone.usuario
+LEFT JOIN SpotifyClone.usuario
 ON historico.usuario_id = usuario.usuario_id
 WHERE usuario.plano_id = 1 OR usuario.plano_id = 4
 GROUP BY historico.music_id
