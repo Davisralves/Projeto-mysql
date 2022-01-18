2d098ce452e2ad23f742bcdd7e9d1aa54a3c30f5
Davisralves <davialves324@gmail.com>
2022-01-18 10:50:05 -0300

    corrigindo requisito 8, 6 e 7

--- desafio6.sql
+++ desafio6.sql
@@ -1,7 +1,7 @@
 SELECT FORMAT(MIN(plano.valor), 2) AS faturamento_minimo,
 MAX(plano.valor) AS faturamento_maximo,
-TRUNCATE(AVG(plano.valor), 2) AS faturamento_medio,
-TRUNCATE(SUM(plano.valor), 2) AS faturamento_total
+ROUND(AVG(plano.valor), 2) AS faturamento_medio,
+ROUND(SUM(plano.valor), 2) AS faturamento_total
 FROM SpotifyClone.plano AS plano
 RIGHT JOIN SpotifyClone.usuario AS usuario
 ON plano.plano_id = usuario.plano_id;
--- desafio8.sql
+++ desafio8.sql
@@ -2,6 +2,7 @@ SELECT artistas.nome AS artista,
 album.album as album
 FROM SpotifyClone.album AS album
 LEFT JOIN SpotifyClone.artistas AS artistas
-ON artistas.artista_id = album.artista_id
+ON artistas.artista_id = album.artista_id WHERE
+artistas.nome = "Walter Phoenix"
 GROUP BY album.album
 ORDER BY album.album;
