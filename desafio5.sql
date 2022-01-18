48e15505528944179ce0c187c995f30679138dab
Davisralves <davialves324@gmail.com>
2022-01-18 11:01:20 -0300

    corrigindo requisito 5 e 7

--- desafio5.sql
+++ desafio5.sql
@@ -1,8 +1,8 @@
-SELECT cancoes.nome AS cancao,
-COUNT(historico.historico_id) AS reproducoes
-FROM SpotifyClone.cancoes AS cancoes
-LEFT JOIN SpotifyClone.historico AS historico
+SELECT cancoes.nome AS `cancao`,
+COUNT(historico.historico_id) AS `reproducoes`
+FROM SpotifyClone.cancoes AS `cancoes`
+LEFT JOIN SpotifyClone.historico AS `historico`
 ON cancoes.cancao_id = historico.music_id
 GROUP BY historico.music_id
-ORDER BY reproducoes DESC, cancao 
+ORDER BY `reproducoes` DESC, `cancao` 
 LIMIT 2;
--- desafio7.sql
+++ desafio7.sql
@@ -1,10 +1,10 @@
-SELECT artistas.nome AS artista,
-album.album as album,
-COUNT(usuario_artista.usuario_id) AS seguidores
-FROM SpotifyClone.album AS album
-LEFT JOIN SpotifyClone.artistas AS artistas
+SELECT artistas.nome AS `artista`,
+album.album as `album`,
+COUNT(usuario_artista.usuario_id) AS `seguidores`
+FROM SpotifyClone.album AS `album`
+LEFT JOIN SpotifyClone.artistas AS `artistas`
 ON artistas.artista_id = album.artista_id
-LEFT JOIN SpotifyClone.usuario_artista AS usuario_artista
+LEFT JOIN SpotifyClone.usuario_artista AS `usuario_artista`
 ON usuario_artista.artista_id = album.artista_id
 GROUP BY album.album
-ORDER BY seguidores DESC, artista, album;
+ORDER BY `seguidores` DESC, `artista`, `album`;
