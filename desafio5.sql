edde339c670645a8cad41739b506bde081ecc68a
Davisralves <davialves324@gmail.com>
2022-01-18 11:31:22 -0300

    corrigindo 5 e 7

--- desafio5.sql
+++ desafio5.sql
@@ -3,6 +3,6 @@ COUNT(historico.historico_id) AS `reproducoes`
 FROM SpotifyClone.cancoes AS `cancoes`
 LEFT JOIN SpotifyClone.historico AS `historico`
 ON cancoes.cancao_id = historico.music_id
-GROUP BY historico.music_id
+GROUP BY cancoes.nome
 ORDER BY `reproducoes` DESC, `cancao` 
 LIMIT 2;
